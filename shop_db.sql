bfb9101ba1f2cc4d84ea56a63ea041265550a051
SarveshMhatre1115 <121401070+SarveshMhatre1115@users.noreply.github.com>
2023-04-25 19:53:41 +0530

    shop_db.sql
    change

--- shop_db.sql
+++ shop_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2022 at 12:51 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Apr 25, 2023 at 04:12 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `admins` (
   `id` int(100) NOT NULL,
   `name` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admins`
@@ -54,7 +54,43 @@ CREATE TABLE `cart` (
   `price` int(10) NOT NULL,
   `quantity` int(10) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `pid`, `name`, `price`, `quantity`, `image`) VALUES
+(7, 2, 1, 'war', 545, 1, '3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games`
+--
+
+CREATE TABLE `games` (
+  `Gname` varchar(50) DEFAULT NULL,
+  `Language` varchar(12) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `ram` int(11) DEFAULT NULL,
+  `system` varchar(10) DEFAULT NULL,
+  `cpu` varchar(30) DEFAULT NULL,
+  `vcard` varchar(30) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `games`
+--
+
+INSERT INTO `games` (`Gname`, `Language`, `size`, `ram`, `system`, `cpu`, `vcard`, `link`) VALUES
+('Grand Theft Auto V', 'English', 64, 6, '7/8/8.1/10', 'core i5 @2.40GHz(4 CPUs)', 'NVIDIA 9800 GT 1GB', 'https://downloads.digitaltrends.com/tencent-gaming-buddy/windows/post-download'),
+('Titanfall 2', 'English', 30, 8, '7<= 64-Bit', 'core i3-6300 (4<= threads)', 'no needed if any have better', 'https://downloads.digitaltrends.com/tencent-gaming-buddy/windows/post-download'),
+('GHOST RECON WILDLANDS', 'English', 60, 8, '7/8/8.1/10', 'core i5-2400S @2.5 GHz', 'NVIDIA 1060 GTx 4GB', 'https://downloads.digitaltrends.com/tencent-gaming-buddy/windows/post-download'),
+('Quantum Break', 'English', 55, 8, '7/8/8.1/10', '2GHz Core i5 or later', 'NVIDIA 1080 GTx 4GB', 'https://downloads.digitaltrends.com/tencent-gaming-buddy/windows/post-download'),
+('Forza Horizon 4', 'English', 70, 16, '10 or Xbox', 'Core i7-3820 @3.6 GHz', 'NVIDIA 1080 GTx 4GB', 'https://downloads.digitaltrends.com/tencent-gaming-buddy/windows/post-download'),
+('Call Of Duty WWII', 'English', 60, 8, '7/8/8.1/10', 'Core i5-3225 @3.3GHz', 'NVIDIA 9800 GTx 2GB', 'https://downloads.digitaltrends.com/tencent-gaming-buddy/windows/post-download');
 
 -- --------------------------------------------------------
 
@@ -69,7 +105,7 @@ CREATE TABLE `messages` (
   `email` varchar(100) NOT NULL,
   `number` varchar(12) NOT NULL,
   `message` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -89,7 +125,15 @@ CREATE TABLE `orders` (
   `total_price` int(100) NOT NULL,
   `placed_on` date NOT NULL DEFAULT current_timestamp(),
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
+(2, 2, 'xyz', '1111111111', 'xyz@gmail.com', 'cash on delivery', 'flat no. xyz city, xyz house, xyz, xyz, xyz - 111111', 'car (145 x 1) - ', 145, '2023-04-20', 'pending'),
+(3, 2, 'sa', '5', 'stylishsm511@gmail.com', 'credit card', 'flat no. axas, 3, wwwwqsqw, WW, wss - 53', 'war (545 x 1) - ', 545, '2023-04-20', 'pending');
 
 -- --------------------------------------------------------
 
@@ -105,7 +149,15 @@ CREATE TABLE `products` (
   `image_01` varchar(100) NOT NULL,
   `image_02` varchar(100) NOT NULL,
   `image_03` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `details`, `price`, `image_01`, `image_02`, `image_03`) VALUES
+(1, 'war', 'sasxAXA', 545, '3.jpg', '3.jpg', '3.jpg'),
+(2, 'car', 'car', 145, 'topgame5.jpg', '3.jpg', 'game.jpg');
 
 -- --------------------------------------------------------
 
@@ -118,7 +170,16 @@ CREATE TABLE `users` (
   `name` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1, 'asd', 'stylishsm511@gmail.com', 'e0c9035898dd52fc65c41454cec9c4d2611bfb37'),
+(2, 'stylishsm51@gmail.c', 'stylishsm51@gmail.com', '011c945f30ce2cbafc452f39840f025693339c42'),
+(3, 'abc', 'abc@gmail.com', 'e0c9035898dd52fc65c41454cec9c4d2611bfb37');
 
 -- --------------------------------------------------------
 
@@ -133,7 +194,14 @@ CREATE TABLE `wishlist` (
   `name` varchar(100) NOT NULL,
   `price` int(100) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `wishlist`
+--
+
+INSERT INTO `wishlist` (`id`, `user_id`, `pid`, `name`, `price`, `image`) VALUES
+(1, 1, 1, 'war', 545, 'i.jpg');
 
 --
 -- Indexes for dumped tables
@@ -195,37 +263,37 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
